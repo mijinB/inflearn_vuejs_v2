0de1635f38fe23f9cfa91b5dd8cb96a5495b05f6
mijinB <bmj13465@gmail.com>
2023-11-23 13:52:21 +0900

    컴포넌트 생성 및 등록

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
 </template>
 
 <script>
-export default {};
+import TodoFooter from "./components/TodoFooter.vue";
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter,
+  },
+};
 </script>
 
 <style>
