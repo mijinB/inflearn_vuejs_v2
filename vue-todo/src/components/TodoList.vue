ecf602d57d1b8a25bdb35dba482193573638dc90
mijinB <bmj13465@gmail.com>
2024-01-08 19:50:11 +0900

    localStorage에 저장된 todo 가져와서 화면에 출력(v-for)

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,68 @@
 <template>
-  <div>list</div>
+    <div>
+        <ul>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+                {{ todoItem }}
+                <span class="removeBtn">
+                  <i class="fa-solid fa-trash"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data: function () {
+        return {
+            todoItems: [],
+        };
+    },
+    created: function () {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+                    //로컬스토리지에 자동으로 저장되는 애들은 제외하고
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    },
+};
 </script>
 
-<style>
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
+.checkBtn {
+    line-height: 45px;
+    /* color: black; */
+    color: #62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted {
+    /* color: #62acde; */
+    color: black;
+}
+.textCompleted {
+    text-decoration: line-through;
+}
 </style>
